5af4a88aa72ad8961cf3430289a31f91cbac39e8
Alexander Swensen <alex.swensen@gmail.com>
2020-10-26 23:31:23 -0500

    try using a passed in token

--- dist/index.js
+++ dist/index.js
@@ -13,17 +13,21 @@ async function main() {
   try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
+    const token = core.getInput('repo-token') || undefined;
+
+    if(!token) {
+      const msg = new Error('A repo-token must be defined.')
+      console.log(msg),
+      core.setFailed(msg);
+    }
+    
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = github.context.payload;
   
-    // to get a octokit context we need, use an environment variable for the token
-    const token =  process.env['GITHUB_TOKEN'];
 
-
-  
     const requestPayload = {
       owner: payload.repository.owner.login,
       repo: payload.repository.name,
