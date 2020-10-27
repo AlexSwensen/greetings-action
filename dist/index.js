4818f33dc6cb41cc69a108621e7545fc9fca66bc
Alexander Swensen <alex.swensen@gmail.com>
2020-10-26 20:11:00 -0500

    add ability to post message

--- dist/index.js
+++ dist/index.js
@@ -8,18 +8,47 @@ module.exports =
 const core = __webpack_require__(186);
 const github = __webpack_require__(438);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+
+async function main() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = github.context.payload;
+  
+    // to get a octokit context we need, use an environment variable for the token
+    const token =  process.env['GITHUB_TOKEN'];
+
+
+  
+    const requestPayload = {
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      issue_number: payload.pull_request.number
+    }
+    console.log('this worked');
+    const client = github.getOctokit(token);
+    console.log('octokit initialized')
+  
+    client.issues.createComment({...requestPayload, body: "foobar"}).then(resp => {
+      console.log('success')
+    }, err => {
+      console.log(err);
+    })
+    
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error);
+  }
+
 }
+
+main();
+
+
 
 /***/ }),
 
